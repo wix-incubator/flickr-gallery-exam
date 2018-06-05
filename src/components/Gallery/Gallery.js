2305dc4fa27f852a87e815c007517fc1bed66db1
GabiG <gabrielg@wix.com>
2018-06-05 15:50:01 +0300

    switch flickr search to 'safe search'

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -25,7 +25,7 @@ class Gallery extends React.Component {
     }
   }
   getImages(tag) {
-    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`;
+    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&safe_search=1&nojsoncallback=1`;
     const baseUrl = 'https://api.flickr.com/';
     axios({
       url: getImagesUrl,
